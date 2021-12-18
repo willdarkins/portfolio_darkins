89976f58631b0dbade94fef256a3125b2add5edd
Will Darkins <willdarkins@gmail.com>
2021-12-17 22:21:40 -0800

    tested numerous amounts of Material UI stuff before landing on following blueprint. Created About component and uploaded ProfilePic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import About from './components/About';
 import './App.css';
-import Button from '@mui/material/Button'
 
 function App() {
   return (
     <>
       <main className='App-header'>
         <About />
-        <Button
-        onClick={() => alert('Fuck You')} 
-        variant='contained' 
-        color='secondary'
-        size='large'>
-          Meet Me, Bro!
-        </Button>
       </main>
     </>
   );
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ProfilePic from "../../assets/ProfilePic.jpg"
 
 function About() {
     return(
         <section>
             <h1>Hi there! I'm Will. </h1>
+            <img src={ProfilePic} style={{width:"100%"}} alt= 'cover' />
         </section>
     )
 }
