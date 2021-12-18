d8c4f38ee25ae74e36baa1df5126297a26c8a97d
Will Darkins <willdarkins@gmail.com>
2021-12-17 22:50:29 -0800

    created some components of nav bar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React from 'react'
 
 function Nav() {
     return (
-
+        <header>
+            <h2>
+                <a href="/">
+                    <span role="img" aria-label="Waving Hand"> 👋</span> WD
+                </a>
+            </h2>
+        </header>
     )
 }
 
 export default Nav
+
+
