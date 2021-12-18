2bd859d07a9c77b227242e343324d2d1cec06b40
Will Darkins <willdarkins@gmail.com>
2021-12-17 22:50:52 -0800

    created some components of nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import About from './components/About';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
   return (
     <>
+    <Nav />
       <main className='App-header'>
         <About />
       </main>
