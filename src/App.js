c4b3588342d55a2b5d8534503502e803f9356842
Will Darkins <willdarkins@gmail.com>
2021-12-18 14:22:03 -0800

    added some text to the projects componenet and imported to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Projects from './components/Projects';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
     <Nav />
       <main className='App-header'>
+        <Projects />
         <About />
       </main>
     </>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 function Projects() {
     return (
-        <>
-            
-        </>
+        <section>
+            <h1>NAME</h1>
+            <p>DESCRIPTION</p>
+        </section>
     )
 }
 
