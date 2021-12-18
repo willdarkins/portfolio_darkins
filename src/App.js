c3b1bb33f445c0b404465dfe49fb493af252bf10
Will Darkins <willdarkins@gmail.com>
2021-12-17 16:22:13 -0800

    finally figured out how to install material UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import About from './components/About';
 import './App.css';
+import Button from '@mui/material/Button'
 
 function App() {
   return (
     <>
       <main>
         <About />
+        <Button>
+          Meet Me, Bro!
+        </Button>
       </main>
     </>
   );
