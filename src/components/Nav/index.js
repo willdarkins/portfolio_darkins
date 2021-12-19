4412696aa88e02cb7ffde2458daf29707e9558d2
Will Darkins <willdarkins@gmail.com>
2021-12-18 20:21:39 -0800

    imported dependencies to create navbar that works off Material UI

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,12 @@
-import React from 'react'
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
 import { capitalizeFirstLetter } from '../../utils/helpers'
 
 function Nav() {
