eb79d6d575f0d22a2cfc95bb2f2c9c6cbb5e13e2
Will Darkins <willdarkins@gmail.com>
2021-12-18 20:29:22 -0800

    added new navbar component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -9,58 +9,124 @@ import {
 import { Link } from "react-router-dom";
 import { capitalizeFirstLetter } from '../../utils/helpers'
 
-function Nav() {
-    //Instead of just listing each category's name, we'll create objects that contain each category's name and a short description
-    const categories = [
-        {
-            name: "front-end",
-            description: "Full stack web projects featuring simple, elegant designs"
-        },
-        {
-            name: "back-end",
-            description: "CLI applications geared towards functionality for CMS projects"
-        },
-        {
-            name: "radio",
-            description: "Samples of my weekly sports radio show in Portland, OR"
-        }
-    ]
-    function categorySelected () {
-            alert("Fuck You")
-    }
-    return (
-        <header>
-            <h2>
-                <a href="/">
-                    <span role="img" aria-label="Waving Hand"> 👋</span> WD
-                </a>
-            </h2>
-            <nav>
-                <ul>
-                    <li>
-                        <a href='#about'>
-                            About Me
-                        </a>
-                    </li>
-                    <li>
-                        <span>
-                            Contact
-                        </span>
-                    </li>
-                    {/* map over the cateogires array to display in the nav bar */}
-                    {categories.map((category) => (
-                        <li key={category.name}>
-                            <span onClick={() => categorySelected()} >
-                            {capitalizeFirstLetter(category.name)}
-                            </span>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
-    )
-}
+const useStyles = makeStyles((theme) => ({
+    navlinks: {
+      marginLeft: theme.spacing(10),
+      display: "flex",
+    },
+   logo: {
+      flexGrow: "1",
+      cursor: "pointer",
+    },
+    link: {
+      textDecoration: "none",
+      color: "white",
+      fontSize: "20px",
+      marginLeft: theme.spacing(20),
+      "&:hover": {
+        color: "yellow",
+        borderBottom: "1px solid white",
+      },
+    },
+  }));
 
-export default Nav
+  const categories = [
+            {
+                name: "front-end",
+                description: "Full stack web projects featuring simple, elegant designs"
+            },
+            {
+                name: "back-end",
+                description: "CLI applications geared towards functionality for CMS projects"
+            },
+            {
+                name: "radio",
+                description: "Samples of my weekly sports radio show in Portland, OR"
+            }
+        ]
+  
+  function Navbar() {
+    const classes = useStyles();
+  
+    return (
+      <AppBar position="static">
+        <CssBaseline />
+        <Toolbar>
+          <Typography variant="h4" className={classes.logo}>
+            Navbar
+          </Typography>
+            <div className={classes.navlinks}>
+              <Link to="/" className={classes.link}>
+                Home
+              </Link>
+              <Link to="/about" className={classes.link}>
+                About
+              </Link>
+              <Link to="/contact" className={classes.link}>
+                Contact
+              </Link>
+              <Link to="/faq" className={classes.link}>
+                FAQ
+              </Link>
+            </div>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+  export default Navbar;
+
+// function Nav() {
+//     //Instead of just listing each category's name, we'll create objects that contain each category's name and a short description
+//     const categories = [
+//         {
+//             name: "front-end",
+//             description: "Full stack web projects featuring simple, elegant designs"
+//         },
+//         {
+//             name: "back-end",
+//             description: "CLI applications geared towards functionality for CMS projects"
+//         },
+//         {
+//             name: "radio",
+//             description: "Samples of my weekly sports radio show in Portland, OR"
+//         }
+//     ]
+//     function categorySelected () {
+//             alert("Fuck You")
+//     }
+//     return (
+//         <header>
+//             <h2>
+//                 <a href="/">
+//                     <span role="img" aria-label="Waving Hand"> 👋</span> WD
+//                 </a>
+//             </h2>
+//             <nav>
+//                 <ul>
+//                     <li>
+//                         <a href='#about'>
+//                             About Me
+//                         </a>
+//                     </li>
+//                     <li>
+//                         <span>
+//                             Contact
+//                         </span>
+//                     </li>
+//                     {/* map over the cateogires array to display in the nav bar */}
+//                     {categories.map((category) => (
+//                         <li key={category.name}>
+//                             <span onClick={() => categorySelected()} >
+//                             {capitalizeFirstLetter(category.name)}
+//                             </span>
+//                         </li>
+//                     ))}
+//                 </ul>
+//             </nav>
+//         </header>
+//     )
+// }
+
+// export default Nav
 
 
