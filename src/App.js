1ce77423ce2a76b73d9080f60c27874b1bfd95ee
Will Darkins <willdarkins@gmail.com>
2021-12-19 10:46:44 -0800

    still having an issue with navbar links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/Nav'
 // import Home from "./pages/home";
 import About from './components/About';
 import Projects from "./components/Projects";
 import './App.css';
+import { Switch } from '@material-ui/core';
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar>
-          <Routes>
-            <Route exact path="/about" element={About} />
-            <Route exact path="/projects" element={Projects} />
-          </Routes>
-        </Navbar>
-        <main className='App-header'>
-        </main>
-      </>
-    </Router>
+    <>
+      <Router>
+        <Navbar />
+      <main className='App-header'>
+        <Switch>
+          <Route path='/about' component={About} />
+          <Route path='/projects' component={Projects} />
+        </Switch>
+      </main>
+      </Router>
+    </>
+
   );
 }
 
