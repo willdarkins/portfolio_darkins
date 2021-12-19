90745889836a3da29f5ae37325095bbe4edb173f
Will Darkins <willdarkins@gmail.com>
2021-12-19 15:32:55 -0800

    created GifList componenet with basic structure to populate all GIFs insdie

---
+++ src/components/GifList/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import gif from '../../assets/GIFS/tech-blog.gif'
+
+function GifList() {
+    return (
+        <>
+        <img src={gif} alt='GIF example' />
+        </>
+    )
+}
+
+export default GifList
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,20 +1,15 @@
 import React from 'react'
 //helper function to capitalize the name value when it's rendered
 import { capitalizeFirstLetter } from '../../utils/helpers'
-import logo from '../../assets/GIFS/employee-tracker.gif';
+import GifList from '../GifList'
 
-function Projects(props) {
-    const currentCategory = {
-        name: "Front-End",
-        description: "Full stack web projects featuring simple, elegant designs"
-    }
+function Projects({currentCategory}) {
+    const {name, description} = currentCategory;
     return (
         <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-            <p>{currentCategory.description}</p>
-            <div className="flex-row">
-                <img src={logo} alt="loading..." className="img-thumbnail mx-1" />
-            </div>
+            <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
+            <p>{description}</p>
+                <GifList />
         </section>
     )
 }
