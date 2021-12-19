2644926bcf7cff63ebeebf20e4ce558def10b40a
Will Darkins <willdarkins@gmail.com>
2021-12-18 16:29:43 -0800

    categories array added to provide better functionality on nav bar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,21 @@
 import React from 'react'
 
 function Nav() {
+    //Instead of just listing each category's name, we'll create objects that contain each category's name and a short description
+    const categories = [
+        {
+            name: "front-end",
+            description: "Full stack web projects featuring simple, elegant designs"
+        },
+        {
+            name: "back-end",
+            description: "CLI applications geared towards functionality for CMS projects"
+        },
+        {
+            name: "broadcasting",
+            description: "Samples of my weekly sports radio show in Portland, OR"
+        }
+    ]
     return (
         <header>
             <h2>
