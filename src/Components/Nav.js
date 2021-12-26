f47a77e81b0175a9adf1b1e197f21f9e1454021a
Will Darkins <willdarkins@gmail.com>
2021-12-26 11:11:42 -0800

    added more links to nav component ;

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -12,13 +12,34 @@ function Nav() {
     return (
         <NavStyles>
             <div className='profile'>
-        <img src= {profile} alt='profile photo' />
-        <ul className='nav-list'>
-            <li className='nav-item'>
-                <NavLink to='/home' activeClassName= 'active-class'>Home</NavLink>
-            </li>
-        </ul>
+                <img src={profile} alt='profile photo' />
             </div>
+            <ul className='nav-list'>
+                <li className='nav-item'>
+                    <NavLink to='/home' activeClassName='active-class'>Home</NavLink>
+                </li>
+                <li className='nav-item'>
+                    <NavLink to='/about' activeClassName='active-class'>About</NavLink>
+                </li>
+                <li className='nav-item'>
+                    <NavLink to='/portfolio' activeClassName='active-class'>Portfolio</NavLink>
+                </li>
+                <li className='nav-item'>
+                    <NavLink to='/resume' activeClassName='active-class'>Resume</NavLink>
+                </li>
+                <li className='nav-item'>
+                    <NavLink to='/social' activeClassName='active-class'>Social</NavLink>
+                </li>
+                <li className='nav-item'>
+                    <NavLink to='/podcasting' activeClassName='active-class'>Podcasting</NavLink>
+                </li>
+                <li className='nav-item'>
+                    <NavLink to='/contact' activeClassName='active-class'>Contact</NavLink>
+                </li>
+            </ul>
+        <footer className='footer'>
+        @2021 Will Darkins
+        </footer>
         </NavStyles>
     )
 }
