d1e94f3d515fe2145b19083b85bb4040196640da
Will Darkins <willdarkins@gmail.com>
2021-12-26 21:18:49 -0800

    created and declared MainStyles variable via styled-components dependency on App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react'
 import SideBar from './Components/SideBar'
 import styled from 'styled-components'
 
-const AppStyled = styled.div`
-    background-color: navy;
-`;
+const MainStyles = styled.main `
+
+`
 
 function App() {
     return (
+        <>
         <SideBar />
+        <MainStyles>
+            
+        </MainStyles>
+
+        </>
     )
 }
+
+
 
 export default App
