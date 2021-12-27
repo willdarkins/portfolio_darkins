18f5e85ab915a87334d745c350ba47f712bdc776
Will Darkins <willdarkins@gmail.com>
2021-12-26 21:53:53 -0800

    created particles background component and config file... working to get the background up and running now

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React from 'react'
 import SideBar from './Components/SideBar'
 import styled from 'styled-components'
 
+//styles to be applied to main content section on the app... this is everything but the SibeBar and Nav
 const MainStyles = styled.main `
+    position: relative;
+    margin-left: 16rem;
+    min-height: 100vh;
+
+    .lines{
+        position: absolute;
+        min-height: 100vh;
+        width: 100%;
+        display: flex;
+        justify-content: space-evenly;
+        .line-1, .line-2, .line-3, .line-4{
+            width: 1px;
+            min-height: 100vh;
+            background-color: red;
+        }
+    }
 
 `
 
@@ -11,7 +28,12 @@ function App() {
         <>
         <SideBar />
         <MainStyles>
-            
+        <div className='lines'>
+            <div className='line-1'></div>
+            <div className='line-2'></div>
+            <div className='line-3'></div>
+            <div className='line-4'></div>
+        </div>
         </MainStyles>
 
         </>
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HomeStyles = styled.header`
+    width: 100%;
+    height: 100vh;
+
+`
+
+function HomePage() {
+    return (
+        <HomeStyles>
+            
+        </HomeStyles>
+    )
+}
+
+export default HomePage
