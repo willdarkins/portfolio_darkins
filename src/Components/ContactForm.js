66bf3a5b624594d6d210b340a246ad6b181d4203
Will Darkins <willdarkins@gmail.com>
2021-12-28 23:42:49 -0800

    created ContactForm component

---
+++ src/Components/ContactForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '@mui/material-ui/core'
+
+function ContactForm() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default ContactForm
