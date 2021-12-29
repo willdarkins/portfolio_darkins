00080cb9caf700da86bddd33c02615d58da0d90a
Will Darkins <willdarkins@gmail.com>
2021-12-28 21:24:33 -0800

    added various icons to represent skills on skills page

---
+++ src/Components/ServiceCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//passing props for info that will be displayed on each of the core skills listed on the About Page
+function ServiceCard({image, title, paragraph}) {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default ServiceCard
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -2,6 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 import { InnerLayout } from '../Styles/Layout'
 import Title from './Title'
+import ServiceCard from '../Components/ServiceCard'
+import fullstack from '../Images/fullstack.png'
+import photoshop from '../Images/photoshop.png'
+import microphone from '../Images/microphone.png'
 
 const SkillStyles = styled.section`
 `
@@ -11,6 +15,13 @@ function Skills() {
         <InnerLayout>
             <SkillStyles>
                 <Title title={'Skills'} />
+                <div className='services'>
+                    <ServiceCard 
+
+                    title={'Full-Stack Development'}
+                    paragraph={' This is some bullshit information about me and things that I do so people will think I know This is some bullshit information about me and things that I do so people will think I know'}
+                    />
+                </div>
             </SkillStyles>
         </InnerLayout>
     )
