9a42a5b366e2b8056e447ff994ade4156827d829
Will Darkins <willdarkins@gmail.com>
2021-12-30 13:30:28 -0800

    got portfolio images to render on PortfolioPage.js using the .map in Menu.js

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,14 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LinkIcon from '@mui/icons-material/Link';
 
 const MenuStyles = styled.div`
 
 `
 
-function Menu({menuItem}) {
+function Menu({ menuItem }) {
     return (
         <MenuStyles>
-            
+            {
+                menuItem.map((item) => {
+                    return <div className='grid-item' key={item.id}>
+                        <div className='portfolio-content'>
+                            <div className='portfolio-image'>
+                                <img src={item.image} />
+                                <ul>
+                                    <li>
+                                        <a href={item.link1}>
+                                            <GitHubIcon />
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href={item.link2}>
+                                            <LinkIcon />
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <h6>{item.title}</h6>
+                            <p>{item.text}</p>
+                        </div>
+                    </div>
+                })
+            }
         </MenuStyles>
     )
 }
--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import {MainLayout, InnerLayout} from '../Styles/Layout'
 import Title from '../Components/Title'
 import portfolios from '../Data/portfolios'
+import Menu from '../Components/Menu'
 
 function Portfolio() {
     const[menuItem, setMenuItem] = useState(portfolios)
@@ -10,7 +11,7 @@ function Portfolio() {
         <MainLayout>
             <Title title={'Portfolio'} />
             <InnerLayout>
-
+                <Menu menuItem={menuItem} />
             </InnerLayout>
         </MainLayout>
     )
