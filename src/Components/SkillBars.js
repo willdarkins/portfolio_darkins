080d68685453280ee2c5258eb413c8c173abfa07
Will Darkins <willdarkins@gmail.com>
2021-12-29 21:30:30 -0800

    progress bars added successfully with styling complete

--- src/Components/SkillBars.js
+++ src/Components/SkillBars.js
@@ -5,7 +5,12 @@ import Title from '../Components/Title'
 import ProgressBar from './ProgressBar'
 
 const SkillBarStyles = styled.section`
-
+    .skills{
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-row-gap: 2rem;
+        grid-column-gap: 3rem;
+    }
 `
 
 function SkillBars() {
@@ -14,14 +19,44 @@ function SkillBars() {
             <MainLayout>
                 <Title title={'Resume'} />
                 <InnerLayout>
-                <div className='skills'>
-                    <ProgressBar 
-                    title={'HTML5'}
-                    text={'70%'}
-                    // width value will be passed as a prop to the ProgressBar.js component
-                    width={'70%'}
-                    />
-                </div>
+                    <div className='skills'>
+                        <ProgressBar
+                            title={'React JS'}
+                            text={'88%'}
+                            // width value will be passed as a prop to the ProgressBar.js component
+                            width={'88%'}
+                        />
+                        <ProgressBar
+                            title={'JavaScript'}
+                            text={'78%'}
+                            // width value will be passed as a prop to the ProgressBar.js component
+                            width={'78%'}
+                        />
+                        <ProgressBar
+                            title={'CSS3'}
+                            text={'75%'}
+                            // width value will be passed as a prop to the ProgressBar.js component
+                            width={'75%'}
+                        />
+                        <ProgressBar
+                            title={'Photoshop'}
+                            text={'72%'}
+                            // width value will be passed as a prop to the ProgressBar.js component
+                            width={'72%'}
+                        />
+                        <ProgressBar
+                            title={'HTML5'}
+                            text={'70%'}
+                            // width value will be passed as a prop to the ProgressBar.js component
+                            width={'70%'}
+                        />
+                        <ProgressBar
+                            title={'Node.js'}
+                            text={'65%'}
+                            // width value will be passed as a prop to the ProgressBar.js component
+                            width={'65%'}
+                        />
+                    </div>
                 </InnerLayout>
             </MainLayout>
         </SkillBarStyles>
