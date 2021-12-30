7326daf75f2cff096ecdc99907dbdb4637aed28b
Will Darkins <willdarkins@gmail.com>
2021-12-30 11:26:30 -0800

    added some basic styling components and properties to PortfolioPage.js and also created js file to store values for images... also uploaded a butt ton of images to but used on the portfolio page.

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -1,10 +1,17 @@
 import React from 'react'
+// import { useState } from 'react'
+import {MainLayout, InnerLayout} from '../Styles/Layout'
+import Title from '../Components/Title'
 
 function Portfolio() {
+    // const[menuItem, setMenuItem] = useState()
     return (
-        <>
-            
-        </>
+        <MainLayout>
+            <Title title={'Portfolio'} />
+            <InnerLayout>
+
+            </InnerLayout>
+        </MainLayout>
     )
 }
 
