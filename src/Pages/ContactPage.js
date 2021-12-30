97c433466479e4a98390faffbc5a1cbc357e5554
Will Darkins <willdarkins@gmail.com>
2021-12-30 01:00:29 -0800

    contact form has been previously created in Material UI... considering making contact cards with the paper components

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import styled from 'styled-components'
-import {MainLayout, InnerLayout} from '../Styles/Layout'
+import { MainLayout, InnerLayout } from '../Styles/Layout'
 import Title from '../Components/Title';
-// import ContactForm from '../Components/ContactForm'
+import ContactForm from '../Components/ContactForm'
 
 const ContactStyles = styled.section`
-
+    .contact-section{
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        position: absolute;
+        left: auto;
+    }
 `
-
 function Contact() {
     return (
         <MainLayout>
             <Title title={'Contact'} />
-            {/* <ContactForm /> */}
+            <ContactStyles>
+                <InnerLayout className={'contact-section'}>
+                    <div className='material-contact-form'>
+                        <ContactForm />
+                    </div>
+                    <div className='contact-panels'>
+
+                    </div>
+                </InnerLayout>
+            </ContactStyles>
         </MainLayout>
     )
 }
