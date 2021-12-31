beeb0b83995e5f7c1c810a666e0e38dc24b74704
Will Darkins <willdarkins@gmail.com>
2021-12-31 11:05:41 -0800

    instagram grid format has been created and images are showing in grid-template-columns format with a 3, 1fr parameter

--- src/Components/SocialGrid.js
+++ src/Components/SocialGrid.js
@@ -3,13 +3,41 @@ import styled from 'styled-components'
 import InstagramIcon from '@mui/icons-material/Instagram';
 
 const SocialStyles = styled.div`
-
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 2rem;
+    .grid-item{
+        .post-content{
+            display: block;
+            position: relative;
+            img{
+                width: 100%;
+            }
+        }
+    } 
 `
 
-function SocialGrid() {
+function SocialGrid({post}) {
     return (
         <SocialStyles>
-            
+            {
+                post.map((i) => {
+                    return <div className='grid-item' key={i.id}>
+                        <div className='post-content'>
+                            <div className='post-image'>
+                                <img src={i.image} alt='' />
+                            </div>
+                            <ul>
+                                <li>
+                                <a href={i.link}>
+                                    <InstagramIcon />
+                                </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                })
+            }
         </SocialStyles>
     )
 }
--- src/Pages/SocialPage.js
+++ src/Pages/SocialPage.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
 import { MainLayout, InnerLayout } from '../Styles/Layout'
 import Title from '../Components/Title'
 import socialPosts from '../Data/socialPosts'
+import SocialGrid from '../Components/SocialGrid'
 
 function Social() {
+    const[post, setPost] = useState(socialPosts)
     return (
-        <>
-            
-        </>
+        <MainLayout>
+            <Title title={'Social'} />
+            <InnerLayout>
+                <SocialGrid post={post} />
+            </InnerLayout>
+        </MainLayout>
     )
 }
 
