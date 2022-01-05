dad7c9eccba641b4d276fb471a276fc713ad0e2e
Will Darkins <willdarkins@gmail.com>
2022-01-05 01:02:09 -0800

    created server and trying to connect my contact form to email using nodemailer

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -22,31 +22,55 @@ const ContactCardStyles = styled.div`
 `
 
 function ContactForm() {
+
+    const [status, setStatus] = useState("Submit");
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setStatus("Sending...");
+      const { firstname, lastname, email, phone, message } = e.target.elements;
+      let details = {
+        firstname: firstname.value,
+        lastname: lastname.value,
+        email: email.value,
+        phone: phone.value,
+        message: message.value,
+      };
+      let response = await fetch("http://localhost:5000/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(details),
+      });
+      setStatus("Submit");
+      let result = await response.json();
+      alert(result.status);
+    };    
     return (
         <ContactCardStyles>
             <Card style={{maxWidth:535, margin:'0 auto'}} className={'card'}>
                 <CardContent>
-                    <Typography gutterBottom variant='h5'>Reach out, and say hello!</Typography>
+                    <Typography gutterBottom variant='h5'>Reach out and say hello!</Typography>
                     <Typography gutterBottom variant='body2' color='textsecondary' component='p'>I'll respond ASAP to questions and work inquires</Typography>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <Grid container spacing={1}>
                             <Grid xs={12} sm={6} item>
-                                <TextField InputLabelProps={{style: { color: 'var(--font-dark)' },}} type='text' label='First Name' placeholder='First Name' variant='outlined' fullWidth required/>
+                                <TextField InputLabelProps={{style: { color: 'var(--font-dark)' },}} type='text' label='First Name' placeholder='First Name' variant='outlined' fullWidth required htmlFor='firstname' />
                             </Grid>
                             <Grid xs={12} sm={6} item>
-                                <TextField InputLabelProps={{style: { color: 'var(--font-dark)' },}} type='text' label='Last Name' placeholder='Last Name' variant='outlined' fullWidth required />
+                                <TextField InputLabelProps={{style: { color: 'var(--font-dark)' },}} type='text' label='Last Name' placeholder='Last Name' variant='outlined' fullWidth required htmlFor='lastname' />
                             </Grid>
                             <Grid xs={12} item>
-                                <TextField InputLabelProps={{style: { color: 'var(--font-dark)' },}} type='email' label='Email' placeholder='Enter Email' variant='outlined' fullWidth required />
+                                <TextField InputLabelProps={{style: { color: 'var(--font-dark)' },}} type='email' label='Email' placeholder='Enter Email' variant='outlined' fullWidth required htmlFor='email' />
                             </Grid>
                             <Grid xs={12} item>
-                                <TextField InputLabelProps={{style: { color: 'var(--font-dark)' },}} type='number' label='Phone #' placeholder='Enter Your Phone #' variant='outlined' fullWidth required />
+                                <TextField InputLabelProps={{style: { color: 'var(--font-dark)' },}} type='number' label='Phone #' placeholder='Enter Your Phone #' variant='outlined' fullWidth required htmlFor='phone' />
                             </Grid>
                             <Grid xs={12} item>
-                                <TextField InputLabelProps={{style: { color: 'var(--font-dark)' },}} type='text' label='Message' multiline rows={4} placeholder='Type Message Here' variant='outlined' fullWidth required />
+                                <TextField InputLabelProps={{style: { color: 'var(--font-dark)' },}} type='text' label='Message' multiline rows={4} placeholder='Type Message Here' variant='outlined' fullWidth required htmlFor='message' />
                             </Grid>
                             <Grid xs={12} item>
-                                <Button type='submit' color='primary' variant='contained' fullWidth>Submit</Button>
+                                <Button type='submit' color='primary' variant='contained' fullWidth>{setStatus}</Button>
                             </Grid>
                         </Grid>
                     </form>
