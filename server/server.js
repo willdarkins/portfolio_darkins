28b6eb873447e67bd91242e43e195c7ab4bae44e
Will Darkins <willdarkins@gmail.com>
2022-01-05 02:20:03 -0800

    trying to get contact form figured out... might have to askBCS tomorrow...

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const nodemailer = require("nodemailer");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/', router)
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`))
 
@@ -27,4 +27,25 @@ const contactEmail = nodemailer.createTransport({
     } else {
       console.log("Ready to Send");
     }
+  });
+
+  router.post("/contact", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message; 
+    const mail = {
+      from: name,
+      to: "***************@gmail.com",
+      subject: "Contact Form Submission",
+      html: `<p>Name: ${name}</p>
+             <p>Email: ${email}</p>
+             <p>Message: ${message}</p>`,
+    };
+    contactEmail.sendMail(mail, (error) => {
+      if (error) {
+        res.json({ status: "ERROR" });
+      } else {
+        res.json({ status: "Message Sent" });
+      }
+    });
   });
