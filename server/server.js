b3b648fe9fe7d8cbab9c9ba122426ddaedb0da1d
Will Darkins <willdarkins@gmail.com>
2022-01-06 11:42:58 -0800

    created .env file to store email and passwords... need to see if it shows up on git...

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const nodemailer = require("nodemailer");
-require('dotenv').config()
+
 
 const app = express();
 app.use(cors());
@@ -9,6 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000
+
+require('dotenv').config()
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
