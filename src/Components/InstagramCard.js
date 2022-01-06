46bc5f733a21ea448b3aad2edbc68f768ba87db3
Will Darkins <willdarkins@gmail.com>
2022-01-05 22:32:06 -0800

    creating Instagram follow card to add to social media page

---
+++ src/Components/InstagramCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Grid, Typography } from '@material-ui/core'
+import styled from 'styled-components';
+
+const InstaCardStyles = styled.div`
+
+`
+
+export const InstagramCard = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
