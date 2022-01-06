b35374c47f5cbb938afcb09857a6a484b5f89f77
Will Darkins <willdarkins@gmail.com>
2022-01-06 11:45:54 -0800

    added the PORT number to my enviornment variables

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.MY_SECRET_PORT
 
 require('dotenv').config()
 
@@ -37,7 +37,7 @@ app.post("/contact", (req, res) => {
   const message = req.body.message;
   const mail = {
     from: firstname,
-    to: "willdarkins@gmail.com",
+    to: process.env.MY_SECRET_EMAIL,
     subject: "Contact Form Submission",
     html: `<p>First Name: ${firstname}</p>
             <p>Last Name: ${lastname}</p>
