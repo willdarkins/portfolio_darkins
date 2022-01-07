c036289695af396dc2aa65b93e53471d772194a0
Will Darkins <willdarkins@gmail.com>
2022-01-06 16:12:13 -0800

    finished social media follow card and descriptor

--- src/Components/InstagramCard.js
+++ src/Components/InstagramCard.js
@@ -5,37 +5,57 @@ import ClearIcon from '@mui/icons-material/Clear';
 import FANProfilePic from '../Images/avatar_FAN.jpg'
 
 const InstaCardStyles = styled.div`
+    display: flex;
+
     .main{
         border: .1rem solid #cccccc;
-        padding: 1rem;
+        padding: .5rem;
         border-radius: .2rem;
         max-width: 200px;
         background-color: var(--dark-card) ;
-    }
-    .header{
-        color: var(--font-dark);
-        height: 0.9rem;
-    }
-    .photo{
-        width: 5rem;
-        height: 5rem;
-        box-shadow: 1px 1px 1px 1px #888888;
-    }
-    .userName{
-        margin: 1.5rem 0 1rem 0;
-        text-align: center;
-        font-weight: bold;
-        color: var(--font-dark);
+        margin-top: 1rem;
+            .header{
+            color: var(--font-dark);
+            height: 0.9rem;
+            }
+            .photo{
+            width: 5rem;
+                height: 5rem;
+                box-shadow: 1px 1px 1px 1px #888888;
+            }
+            .userName{
+                margin: 1.5rem 0 1rem 0;
+                text-align: center;
+                font-weight: bold;
+                color: var(--font-dark);
 
+                }
+            .profName{
+                color: var(--font-dark);
+            }
+            .button{
+                text-transform: none;
+                padding-left: 1rem;
+                padding-right: 1rem;
+                margin-bottom: 1rem;
+                a{
+                    font-size: .9rem;
+                }
+            }
     }
-    .button{
-        text-transform: none;
-        padding-left: 1rem;
-        padding-right: 1rem;
-        a{
-            font-size: .9rem;
-        }
+    .descrip-content{
+            margin-left: 5rem;
+            margin-top: 2.5rem;
+            h4{
+                font-size: 2.5rem;
+                color: var(--font-dark);
+            }
+            .paragraph{
+                padding: 1rem 0;
+                color: var(--font-dark);
+            }
     }
+
 `
 
 export const InstagramCard = () => {
@@ -48,6 +68,7 @@ export const InstagramCard = () => {
                 <ProfileName />
                 <FollowButton />
             </Grid>
+            <Descrpitor />
         </InstaCardStyles>
     )
 }
@@ -79,14 +100,29 @@ const UserName = () => {
 }
 const ProfileName = () => {
     return (
-        <Typography color='textSecondary' variant='caption'>1080 The FAN</Typography>
+        <Typography className='profName' variant='caption'>1080 The FAN</Typography>
     )
 }
 const FollowButton = () => {
     return (
-        <Button color= "primary" variant='contained' size='small' className='button'>
+        <Button color="primary" variant='contained' size='small' className='button'>
             <a href='https://www.instagram.com/1080thefan/?hl=en' target='_blank' rel='noopener noreferrer'>
-                           Follow </a>
-        </Button> 
+                Follow </a>
+        </Button>
+    )
+}
+
+const Descrpitor = () => {
+    return (
+        <div className='descrip-content'>
+            <h4>Creating for the Airwaves</h4>
+            <p className='paragraph'>
+                Not only do I produce audio for the ESPN Radio affiliate in Portland OR, 1080 The FAN,
+                I also serve as Digital Content Manager for the brand. <br/><br/>My responsibilities include currating 
+                design and tone for social media channels, producing on-air promotional videos and creating
+                awesome sports graphics to get listeners excited about our brand. Check out some of my work, below!
+                Don't forget to follow the station!
+            </p>
+        </div>
     )
 }
