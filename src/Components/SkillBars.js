27b60bb412166ed26f139878b6637338c5028220
Will Darkins <willdarkins@gmail.com>
2022-01-07 20:49:01 -0800

    converted the skill bars to a single column grid layout at 700px width

--- src/Components/SkillBars.js
+++ src/Components/SkillBars.js
@@ -10,6 +10,9 @@ const SkillBarStyles = styled.section`
         grid-template-columns: repeat(2, 1fr);
         grid-row-gap: 2rem;
         grid-column-gap: 3rem;
+        @media screen and (max-width: 700px){
+        grid-template-columns: repeat(1, 1fr);
+}
     }
 `
 
