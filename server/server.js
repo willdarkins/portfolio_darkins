609b204d397160d4b93cbdcd1f5126852564d674
Will Darkins <willdarkins@gmail.com>
2022-01-09 00:45:04 -0800

    added some files

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const path = require('path')
 const cors = require('cors');
 const nodemailer = require("nodemailer");
+const path = require('path')
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || '5000';
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 require('dotenv').config()
 
