0b9eb2d3f7174f371de69982f8c2fe51b18ebfb6
Will Darkins <willdarkins@gmail.com>
2022-01-09 12:33:09 -0800

    took out cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path')
-const cors = require('cors');
+// const cors = require('cors');
 const nodemailer = require("nodemailer");
 const path = require('path')
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
