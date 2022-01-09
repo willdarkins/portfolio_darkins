c53c432b1118e408bed87afabb238d8c95acf95c
Will Darkins <willdarkins@gmail.com>
2022-01-09 13:43:33 -0800

    changed port... again

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || '5001';
+const PORT = process.env.PORT || '5000';
 
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 
