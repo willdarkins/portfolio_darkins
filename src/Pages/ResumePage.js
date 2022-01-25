550a8040f2afb9f9d543ca36b0e6abb6f636ab00
Will Darkins <willdarkins@gmail.com>
2022-01-24 19:33:26 -0800

    took out skill bar section

--- src/Pages/ResumePage.js
+++ src/Pages/ResumePage.js
@@ -20,7 +20,7 @@ function ResumePage() {
                     }
                 },
             }}>
-                <SkillBars />
+                {/* <SkillBars /> */}
                 <Resume />
             </motion.div>
         </MainLayout>
